019db8278b37092baeeb5519e03753a19ba37f40
minleg <minleg@minleg.com>
2022-04-27 19:38:11 -0500

    update

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 let express = require('express')
 let routes = require('./routes/facts.js')
+let path = require('path')
 
 let app = express()
 
+app.use(express.static(path.join(__dirname, 'content'))) // serves this file as a static content
+
 app.use('/api', routes)
+
+// error handlers
+app.use(function(req, res, next){
+    res.status(404).send('Not found')
+})
+
+app.use(function(err, req, res, next){ 
+    console.error('Request to ' + req.originalUrl + ' errored because\n', err)
+    res.status(500).send('Server error')
+})
 
 let server = app.listen(process.env.PORT || 3000, function() {
     console.log('app running on port', server.address().port)
