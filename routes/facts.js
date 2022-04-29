f6d4e2cab592e4af02bc87d7afca17d760af12c0
minleg <minleg@minleg.com>
2022-04-29 12:13:32 -0500

    update

--- routes/facts.js
+++ routes/facts.js
@@ -1,5 +1,6 @@
 
 let express = require('express')
+const { route } = require('express/lib/application')
 let router = express.Router()
 
 let stateData = require('./state_fact.json')
@@ -12,6 +13,7 @@ router.get('/about', function(req, res, next) {
 
 router.get('/state-list', function(req, res, next) {
     let stateNames = Object.keys(stateData)  // array of all the keys from the object
+    console.log(stateNames)
     res.json(stateNames)
 })
 
@@ -34,5 +36,41 @@ router.get('/fact/:stateName', function(req, res, next){
     */
 })
 
+/*
+router.get('/fact/:stateInitial', function(req,res,next){
+    let stateInitial = req.params.stateInitial
+    let stateNames = Object.keys(stateData)
+    for( let stateindex in stateNames){
+        let state = stateNames[stateindex]
+        if(state.startsWith(stateInitial){
+            let fact = stateData[state]
+            if(fact){
+                res.json({name: state, fact: fact})
+            } else {
+                res.send(404).send('No state with such initial')
+            }
+        })
+    }
+})
+*/
 
+router.get('/fact/letter/:letter', function(req, res, next){
+    let initial = req.params.letter
+    if(initial.length > 1){ // checks if the length of state initial that user entered is greater than one
+        initial = initial[0].toUpperCase() + initial.slice(1) // capitalises the first character since all States initials are capitilized, result of mi or Mi would result in same
+    } else {
+        initial = initial.toUpperCase() // makes sure the single letter user entered is capitalised.
+    }
+    let states = {}
+    for(state in stateData){
+        if(state.startsWith(initial)){
+            states[state] = stateData[state]
+        }
+    }
+    if(Object.keys(states).length === 0) { // checks if states object is empty
+        res.status(404).send('State not found') // returns this states not found when the letter entered doesn't match any states like /qwerty or /1233
+    } else {
+        res.json(states)
+    }
+})
 module.exports = router
